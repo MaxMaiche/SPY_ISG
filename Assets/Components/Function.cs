160faff5f38d1e912f4df6bf0aeb8fcaaeb7e83d
Iordache Paul-Tiberiu <iordache.tiberiu19@yahoo.com>
2024-10-20 16:01:08 +0200

    ajout du bloc fonction sur creer scenario

---
+++ Assets/Components/Function.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Function : BaseElement {
+	// Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
+    public enum ActionType { Function };
+    public ActionType actionType;
+}
