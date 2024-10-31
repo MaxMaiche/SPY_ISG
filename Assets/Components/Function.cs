43d73a165a6832178eb269afb7ff6c34460ded1f
Max Maiche <max.maiche@gmail.com>
2024-10-31 15:43:49 +0100

    Ajout Panel droit + pref Function avec text input

--- Assets/Components/Function.cs
+++ Assets/Components/Function.cs
@@ -4,4 +4,5 @@ public class Function : BaseElement {
 	// Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
     public enum ActionType { Function };
     public ActionType actionType;
+    public string functionName;
 }
