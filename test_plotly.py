247547ce2552e0f828a4d15842a12c935b213b07
Max Maiche <max.maiche@gmail.com>
2024-12-17 16:52:27 +0100

    2 bottom plots

--- test_plotly.py
+++ test_plotly.py
@@ -5,7 +5,9 @@ from datetime import datetime
 from urllib.parse import urljoin
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+import plotly.express as px
 from plotly.subplots import make_subplots
+import xml.etree.ElementTree as ET
 
 
 
@@ -20,6 +22,9 @@ headers = {
     "X-Experience-API-Version": XAPI_VERSION,
     "Content-Type": "application/json"
 }
+
+# Streaming asset path
+streaming_asset_path = "Assets/StreamingAssets/"
 
 # Store session data
 launched_data = []
@@ -51,7 +56,7 @@ def fetch_data(verb):
     "agent": json.dumps({
         "account": {
             "homePage": "https://www.lip6.fr/mocah/",
-            "name": "3D37C851"
+            "name":"3D37C851"
         }
     }),
     # check vocabulary lrs
@@ -72,12 +77,17 @@ def fetch_data(verb):
         return None
     
 # Subplots
-def plot_with_subplots(levels, max_scores, min_scores, avg_execution_times, max_execution_times, min_execution_times):
+def plot_with_subplots(levels, max_scores, min_scores, avg_execution_times, max_execution_times, min_execution_times, level_scores, cpt_lunched):
     # Create a figure with two side-by-side subplots
     fig = make_subplots(
-        rows=1, cols=2,  # 1 row, 2 columns for side-by-side layout
-        subplot_titles=("Max and Min Scores Per Level", "Execution Times Per Level"),
-        horizontal_spacing=0.2  # Increase spacing between the two plots
+        rows=2, cols=2,  # 1 row, 2 columns for side-by-side layout
+        subplot_titles=("Max and Min Scores Per Level (Divided by the level max score)", 
+                        "Execution Times Per Level", 
+                        "Scores Distribution Per Level",
+                        "Number of launched per level"
+                        ),  
+        horizontal_spacing=0.2,  # Increase spacing between the two plots
+        vertical_spacing= 0.3 # Increase spacing between the two plots
     )
 
     # Left subplot: Scores
@@ -104,19 +114,45 @@ def plot_with_subplots(levels, max_scores, min_scores, avg_execution_times, max_
         row=1, col=2
     )
 
+    # Bottom left subplot: Violine scores plot
+    for level, scores in level_scores.items():
+        fig.add_trace(
+            go.Box(y=scores, name=level, boxmean=True, showlegend=False),
+            row=2, col=1
+        )
+
+    # Bottom right subplot: Number of launched per level
+    fig.add_trace(
+        go.Bar(x=list(cpt_lunched.keys()), y=list(cpt_lunched.values()), name='Number of launched', marker_color='orange', showlegend=False),
+        row=2, col=2
+    )
+
     # Add borders around each graph
-    fig.add_shape(
-        type="rect",
-        xref="paper", yref="paper",  # Use paper coordinates for layout-level shapes
-        x0=0.0, x1=0.4, y0=0, y1=1,  # Left graph border
-        line=dict(color="black", width=2)  # Border color and width
-    )
-    fig.add_shape(
-        type="rect",
-        xref="paper", yref="paper",  # Use paper coordinates for layout-level shapes
-        x0=0.6, x1=1.0, y0=0, y1=1,  # Right graph border
-        line=dict(color="black", width=2)  # Border color and width
-    )
+    # fig.add_shape(
+    #     type="rect",
+    #     xref="paper", yref="paper",  # Use paper coordinates for layout-level shapes
+    #     x0=0.0, x1=0.4, y0=0, y1=0.3,  # Left graph border
+    #     line=dict(color="black", width=2)  # Border color and width
+    # )
+    # fig.add_shape(
+    #     type="rect",
+    #     xref="paper", yref="paper",  # Use paper coordinates for layout-level shapes
+    #     x0=0.6, x1=1.0, y0=0, y1=0.3,  # Right graph border
+    #     line=dict(color="black", width=2)  # Border color and width
+    # )
+
+    # fig.add_shape(
+    #     type="rect",
+    #     xref="paper", yref="paper",  # Use paper coordinates for layout-level shapes
+    #     x0=0.0, x1=0.4, y0=0.6, y1=1,  # Left graph border
+    #     line=dict(color="black", width=2)  # Border color and width
+    # )
+    # fig.add_shape(
+    #     type="rect",
+    #     xref="paper", yref="paper",  # Use paper coordinates for layout-level shapes
+    #     x0=0.6, x1=1.0, y0=0.6, y1=1,  # Right graph border
+    #     line=dict(color="black", width=2)  # Border color and width
+    # )
 
     # Update axis titles
     fig.update_xaxes(title_text="Level Name", row=1, col=1)
@@ -124,6 +160,9 @@ def plot_with_subplots(levels, max_scores, min_scores, avg_execution_times, max_
 
     fig.update_xaxes(title_text="Level Name", row=1, col=2)
     fig.update_yaxes(title_text="Execution Times (s)", row=1, col=2)
+
+    fig.update_xaxes(title_text="Level Name", row=2, col=1)
+    fig.update_yaxes(title_text="Scores", row=2, col=1)
 
     # Layout adjustments
     fig.update_layout(
@@ -133,7 +172,7 @@ def plot_with_subplots(levels, max_scores, min_scores, avg_execution_times, max_
         title_x=0.5,
         barmode='group',
         template='plotly_white',
-        height=500,  # Adjust height for better visualization
+        height=1000,  # Adjust height for better visualization
         width=1920,  # Set width for full-screen visualization
         margin=dict(t=80, b=50, l=20, r=20),  # Margins
     )
@@ -141,7 +180,7 @@ def plot_with_subplots(levels, max_scores, min_scores, avg_execution_times, max_
     # Ensure bars take up space effectively
     fig.update_layout(
         bargap=0.1,  # Adjust spacing between bars
-        bargroupgap=0.1  # Adjust spacing between groups
+        bargroupgap=0.12  # Adjust spacing between groups
     )
 
     fig.add_annotation(
@@ -160,7 +199,17 @@ def plot_with_subplots(levels, max_scores, min_scores, avg_execution_times, max_
     # Show the figure
     fig.show()
 
+def parse_xml(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    scores = []
+    for child in root:
+        if child.tag == 'score':
+            scores.append(child.attrib)
+    return scores
+
 def main():
+
     print("Fetching launched data...")
     launched_response = fetch_data("http://adlnet.gov/expapi/verbs/launched")
     if launched_response:
@@ -185,7 +234,7 @@ def main():
     # Match completed with the closest launched before it
     level_scores = {}
     level_execution_times = {}
-
+    cpt_lunched = {}
     for completed in completed_data:
         completed_timestamp = datetime.fromisoformat(completed['timestamp'].replace("Z", "+00:00"))
         
@@ -203,16 +252,27 @@ def main():
             launched_timestamp = datetime.fromisoformat(launched['timestamp'].replace("Z", "+00:00"))
             if launched_timestamp <= completed_timestamp:
                 matching_launched = launched
-                break
 
+                break
+        
+        
         if matching_launched:
             # Extraire le nom du niveau
             level_name = matching_launched['object']['definition']['extensions']['https://spy.lip6.fr/xapi/extensions/value'][0]
+            level_xml_path = streaming_asset_path + level_name 
+
+            # Incrémentation du nombre de lancement
+            if level_name not in cpt_lunched:
+                cpt_lunched[level_name] = 0
+            cpt_lunched[level_name] += 1
             
+            stars_scores = parse_xml(level_xml_path)[0]
+            max_score = int(stars_scores['threeStars'])
             # Ajouter le score au niveau correspondant
             if level_name not in level_scores:
                 level_scores[level_name] = []
-            level_scores[level_name].append(completed_score)
+            level_scores[level_name].append(completed_score/max_score)
+
 
             # Calculer le temps d'exécution
             execution_time = (completed_timestamp - launched_timestamp).total_seconds()
@@ -232,7 +292,7 @@ def main():
     max_execution_times = [max(times) if times else 0 for times in level_execution_times.values()]
     min_execution_times = [min(times) if times else 0 for times in level_execution_times.values()]
 
-    plot_with_subplots(levels, max_scores, min_scores, avg_execution_times, max_execution_times, min_execution_times)
+    plot_with_subplots(levels, max_scores, min_scores, avg_execution_times, max_execution_times, min_execution_times, level_scores, cpt_lunched)
 
 
 
