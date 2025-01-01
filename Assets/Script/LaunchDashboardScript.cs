1714cd0aa4c04f61187187705770f204c36a38ee
Max Maiche <max.maiche@gmail.com>
2025-01-01 17:31:27 +0100

    Bouton Dahsboard in Spy

---
+++ Assets/Script/LaunchDashboardScript.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Diagnostics;
+using UnityEngine;
+using TMPro;
+
+
+class Program: MonoBehaviour
+{
+    public TMP_InputField inputField;
+
+    void Start()
+    {
+
+    }
+
+    public void launchPythonScript()
+    {
+        String arg = inputField.text;
+
+        // Chemin vers l'exécutable Python
+        string pythonPath = @"C:\Users\Max\AppData\Local\Microsoft\WindowsApps\python3.11.exe";
+
+        // Chemin vers le script Python
+        string scriptPath = @"dashboard_plotly.py";
+
+        // Arguments à passer au script Python
+        string arguments = $"\"{scriptPath}\" {arg}";
+
+        // Configuration du processus
+        ProcessStartInfo psi = new ProcessStartInfo
+        {
+            FileName = pythonPath,
+            Arguments = arguments,
+            RedirectStandardOutput = false,
+            RedirectStandardError = false,
+            UseShellExecute = true,
+            CreateNoWindow = false
+        };
+
+        // Lancement du processus
+        Process process = new Process
+        {
+            StartInfo = psi
+        };
+        process.Start();
+    }
+}
